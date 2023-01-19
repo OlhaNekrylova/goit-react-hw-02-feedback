4f345909e5d7badcc2b562803843794726012ce5
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-01-19 21:31:00 +0200

    add propTypes in package.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  countTotalFeedback = () => {};
+
+  countPositiveFeedbackPercentage = () => {};
+
+  render () {
+    return (
+      <>
+      
+        <Section className="sectionStatistics"></Section>
+      </>
+      
+    )
+  };
 };
+
+export default App;
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+class Section extends React.Component {
+    static propTypes = {
+    
+    };
+
+    render () {
+        return (
+            <SectionTitle text="Statistics" />
+            <Statistics good={} neutral={} bad={} total={} positivePercentage={} /> 
+        
+        )
+    };
+};
+
+export default Section;
